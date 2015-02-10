76a1299c65176e80b2d60394e8dcf416dc2e5044
Kyle Barton <kylebarton@Kyles-MacBook-Pro.local>
2015-02-09 18:58:37 -0800

    Trying to figure out how my brain will communicate with my frontend.

---
+++ app.js
@@ -0,0 +1,6 @@
+//This is the controller I guess?
+
+angular.module("brainApp", [])
+	.controller("chessController", function($scope, $http){
+		
+	})
--- brain.js
+++ brain.js
@@ -6,14 +6,34 @@ var chess = require('./ChessLogic/chess')
 function simRandomGame(){
 	var game = new chess.Chess();
 	while(!game.game_over()){
-		console.log("Position\n " + game.ascii() + '\n');
-		var moves = game.moves();
-		var move = moves[Math.floor(Math.random() * moves.length)]
-		game.move(move);
-		console.log("move: " + move + '\n');
-		//console.readLine();
+		game.move(nextMove(game));
+		console.log(game.ascii());
+		// console.log("Position\n " + game.ascii() + '\n');
+		// var moves = game.moves();
+		// var move = moves[Math.floor(Math.random() * moves.length)]
+		// game.move(move);
+		// console.log("move: " + move + '\n');
+		// //console.readLine();
 	}
 }
 
+function nextMove(game){
+	var moves = game.moves();
 
-simRandomGame();
+	var move = moves[Math.floor(Math.random() * moves.length)];
+
+	return move;
+}
+
+var bigGame = new chess.Chess();
+
+function initGame(){
+	return new chess.Chess();
+}
+
+function moveNext(){
+	bigGame.move(nextMove(bigGame));
+	return bigGame.ascii();
+}
+
+//Ok, let's treat this like a server for now.
