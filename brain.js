1dbf0666ba4efeb8d4bc6c182bf3f1386f44c672
Kyle Barton <kjbarton4@gmail.com>
2015-02-12 12:47:12 -0800

    Serving static content yo.

--- brain.js
+++ brain.js
@@ -7,6 +7,8 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname));
+
 app.get('/', function(req, res){
 	res.send(__dirname); //+ '/index.html');
 })
@@ -20,7 +22,7 @@ app.get('/game', function(req, res){
 app.listen(3000);
 
 console.log('Listening on port 3000');
-console.log(__dirname + '/index.html');
+//console.log(__dirname + '/index.html');
 
 
 
