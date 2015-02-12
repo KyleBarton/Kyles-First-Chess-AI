85117e94db5b53c0fd599c3733440a2c25be384e
Kyle Barton <kjbarton4@gmail.com>
2015-02-11 23:33:48 -0800

    Need to rework my angular stuff.

--- app.js
+++ app.js
@@ -2,5 +2,9 @@
 
 angular.module("brainApp", [])
 	.controller("chessController", function($scope, $http){
-		
+		$http.get('/game').success(function(data){
+			$scope.currentBoard = data;
+		}).fail(function(data){
+			$scope.currentBoard = 'Nope!';
+		})
 	})
--- brain.js
+++ brain.js
@@ -3,37 +3,65 @@
 var util = require('util')
 var chess = require('./ChessLogic/chess')
 
-function simRandomGame(){
-	var game = new chess.Chess();
-	while(!game.game_over()){
-		game.move(nextMove(game));
-		console.log(game.ascii());
-		// console.log("Position\n " + game.ascii() + '\n');
-		// var moves = game.moves();
-		// var move = moves[Math.floor(Math.random() * moves.length)]
-		// game.move(move);
-		// console.log("move: " + move + '\n');
-		// //console.readLine();
-	}
-}
+var express = require('express');
 
-function nextMove(game){
-	var moves = game.moves();
+var app = express();
 
-	var move = moves[Math.floor(Math.random() * moves.length)];
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+})
 
-	return move;
-}
 
-var bigGame = new chess.Chess();
+app.get('/game', function(req, res){
+	var game = chess.Chess();
+	res.send(game.asii());
+})
 
-function initGame(){
-	return new chess.Chess();
-}
+app.listen(3000);
 
-function moveNext(){
-	bigGame.move(nextMove(bigGame));
-	return bigGame.ascii();
-}
+console.log('Listening on port 3000');
+console.log(__dirname + '/index.html');
+
+
+
+
+
+
+
+
+// function simRandomGame(){
+// 	var game = new chess.Chess();
+// 	while(!game.game_over()){
+// 		game.move(nextMove(game));
+// 		console.log(game.ascii());
+// 		// console.log("Position\n " + game.ascii() + '\n');
+// 		// var moves = game.moves();
+// 		// var move = moves[Math.floor(Math.random() * moves.length)]
+// 		// game.move(move);
+// 		// console.log("move: " + move + '\n');
+// 		// //console.readLine();
+// 	}
+// }
+
+// function nextMove(game){
+// 	var moves = game.moves();
+
+// 	var move = moves[Math.floor(Math.random() * moves.length)];
+
+// 	return move;
+// }
+
+// var bigGame = new chess.Chess();
+
+// function initGame(){
+// 	return new chess.Chess();
+// }
+
+// function moveNext(){
+// 	bigGame.move(nextMove(bigGame));
+// 	return bigGame.ascii();
+// }
 
 //Ok, let's treat this like a server for now.
+
+
