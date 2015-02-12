4f281f6b68a58c32e7a66ed7206bcedfb67c3026
KyleBarton <kbarton@incomm.com>
2015-02-12 08:41:16 -0800

    Ok, I have my angular controller working. Going to slowly integrate with the backend and try to serve an ascii chess board.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 //This is the controller I guess?
 
-angular.module("brainApp", [])
-	.controller("chessController", function($scope, $http){
-		$http.get('/game').success(function(data){
-			$scope.currentBoard = data;
-		}).fail(function(data){
-			$scope.currentBoard = 'Nope!';
-		})
-	})
+var brainApp = angular.module("brainApp", []);
+
+
+
+brainApp.controller('chessController', function($scope){
+	$scope.currentBoard = 5;
+});
+	// .controller("chessController", function($scope, $http){
+	// 	$scope.currentBoard = 6;
+	// 	// $http.get('/game').success(function(data){
+	// 	// 	$scope.currentBoard = data;
+	// 	// }).fail(function(data){
+	// 	// 	$scope.currentBoard = 'Nope!';
+	// 	// })
+	// })
