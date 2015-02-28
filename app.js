4f0ad5f1943fc5aceec6c2e6f0c4713a695510c9
Kyle Barton <kjbarton4@gmail.com>
2015-02-27 23:51:47 -0800

    I can not look at a random game being played using the chessboardjs styling library. Time to make the game smart!

--- app.js
+++ app.js
@@ -3,29 +3,31 @@ var brainApp = angular.module("brainApp", []);
 
 
 brainApp.controller('chessController', function($scope, $http){
-
+	$scope.errorMessage = "";
+	
 	$http.get('/newGame').success(function(data){
 		$scope.gameFen = data;
 	}).error(function(err){
-		$scope.gameFen = 'could not find endpoint';
+		$scope.errorMessage = 'could not find endpoint';
 	});
 
-	$http.get('/ascii?game=' + $scope.gameFen).success(function(data){
-		$scope.gameAscii = data;
-	}).error(function(err){
-		$scope.gameAscii = "Ascii failed to load";
-	});
+	// $http.get('/ascii?game=' + $scope.gameFen).success(function(data){
+	// 	$scope.gameAscii = data;
+	// }).error(function(err){
+	// 	$scope.errorMessage = "Ascii failed to load";
+	// });
 	
 	$scope.nextMove = function(){
 		$http.get('/nextMove?game=' + $scope.gameFen).success(function(data){
 			$scope.gameFen = data;
 			$http.get('/ascii?game=' + $scope.gameFen).success(function(data){
 				$scope.gameAscii = data;
+				var board1 = new ChessBoard('board1', $scope.gameFen);
 			}).error(function(err){
-				$scope.gameAscii = "Ascii failed to load";
+				$scope.errorMessage = "Ascii failed to load";
 			});
 		}).error(function(err){
-			$scope.gameFen = "Could not get next game";
+			$scope.errorMessage = "Could not get next game";
 		});
 	};
 });
