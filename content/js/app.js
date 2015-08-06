12deaa6604117b4862964db905f2d7c9f6e1d30d
Kyle Barton <kjbarton4@gmail.com>
2015-08-05 22:55:03 -0700

    Cleaned up the html/ng-controller a little bit. Still need to decouple, and then add logic for sim-ing a whole game without the button clicks.

--- content/js/app.js
+++ content/js/app.js
@@ -3,33 +3,23 @@ var brainApp = angular.module("brainApp", []);
 
 
 brainApp.controller('chessController', function($scope, $http){
-	$scope.errorMessage = "";
-	
-	$http.get('/newGame').success(function(data){
-		$scope.gameFen = data;
-	}).error(function(err){
-		$scope.errorMessage = 'could not find endpoint';
-	});
-
-	// $http.get('/ascii?game=' + $scope.gameFen).success(function(data){
-	// 	$scope.gameAscii = data;
-	// }).error(function(err){
-	// 	$scope.errorMessage = "Ascii failed to load";
-	// });
-	
+	//todo: we need to break this out into a service
 	$scope.nextMove = function(){
 		$http.get('/nextMove?game=' + $scope.gameFen).success(function(data){
 			$scope.gameFen = data;
-
-			$http.get('/ascii?game=' + $scope.gameFen).success(function(data){
-				$scope.gameAscii = data;
-				// debugger;
-				//var board1 = new ChessBoard('board1', $scope.gameFen);
+				var board1 = new ChessBoard('board1', $scope.gameFen);
 			}).error(function(err){
-				$scope.errorMessage = "Ascii failed to load";
-			});
-		}).error(function(err){
 			$scope.errorMessage = "Could not get next game";
 		});
 	};
+
+	$scope.newGame = function(){
+		$http.get('/newGame').success(function(data){
+			$scope.gameFen = data;
+		}).error(function(err){
+			$scope.errorMessage = 'could not find endpoint';
+		});
+	}
+
+
 });
