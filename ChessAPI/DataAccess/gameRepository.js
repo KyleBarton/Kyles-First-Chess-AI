c285e692fb51d4d9df8142301c1d8ed98caa2fc0
Kyle Barton <kjbarton4@gmail.com>
2016-07-05 16:04:50 -0700

    building out functionality to retrieve a saved game.

--- ChessAPI/DataAccess/gameRepository.js
+++ ChessAPI/DataAccess/gameRepository.js
@@ -6,7 +6,14 @@ var dbConnectError = 'Could not connect to mongo database';
 
 var gameRepository = {
 	getData: function(id, callBack){
-
+		mongoClient.connect(dburl, function(err, db){
+			if (err){
+				throw dbConnectError;
+			}
+			db.collection('Games').find({"_id": ObjectId(id)}, function(err, records){
+				console.log(records);
+			})
+		});
 	},
 	saveNew: function(fen, callBack){
 		mongoClient.connect(dbUrl, function(err, db){
