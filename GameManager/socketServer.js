24600f060e52978a09105f0dda73573977fd46bf
Kyle Barton <kjbarton4@gmail.com>
2016-07-09 14:10:30 -0700

    socket server is now using the chess API in order to create a new game

--- GameManager/socketServer.js
+++ GameManager/socketServer.js
@@ -1,17 +1,46 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http');
+var httpServer = require('http').Server(app);
+var io = require('socket.io')(httpServer);
 
 app.use(express.static('../TestClient'));
 
+var createNewGame = function(callBack){
+	var options = {
+		"host": "localhost",
+		"port": "3001",
+		"path": "/games",
+		"method": "POST"
+	}
+	var request = http.request(options, function(response){
+		response.setEncoding('utf8');
+		response.on('data', callBack);
+	});
+	request.write("");
+	request.end();
+}
+
 io.on('connection', function(socket){
+	
 	console.log('a user connected');
+	
+	socket.on('new game', function(data){
+		createNewGame(function(game){
+			socket.emit('created game', JSON.parse(game))
+		})
+		// socket.emit('created game', {
+		// 	"fen": "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+		// 	"id": "57815edcb23dd6ce3e495050",
+		// 	"gameOver": false
+		// });
+	});
+	
 	socket.on('disconnect', function(){
 		console.log('a user disconnected');
 	})
 });
 
-http.listen("3002", function(){
+httpServer.listen("3002", function(){
 	console.log("Listening on port 3002");
 });
