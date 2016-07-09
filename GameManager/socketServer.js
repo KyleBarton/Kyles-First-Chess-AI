0ef2c3eaf0fe03f05e8d5ff47230a752d214bae5
Kyle Barton <kjbarton4@gmail.com>
2016-07-09 15:18:42 -0700

    moving the gameManager logic out of the socket server. Also, deleting a console log that's been around waaaaaayyy too long.

--- GameManager/socketServer.js
+++ GameManager/socketServer.js
@@ -1,77 +1,30 @@
 var express = require('express');
 var app = express();
-var http = require('http');
 var httpServer = require('http').Server(app);
 var io = require('socket.io')(httpServer);
+var gameManager = require('./gameManager');
 
 app.use(express.static('../TestClient'));
 
-var createNewGame = function(callBack){
-	var options = {
-		"host": "localhost",
-		"port": "3001",
-		"path": "/games",
-		"method": "POST"
-	};
-	var request = http.request(options, function(response){
-		response.setEncoding('utf8');
-		response.on('data', function(data){
-			callBack(JSON.parse(data));
-		});
-	});
-	request.write("");
-	request.end();
-}
-
-var getGame = function(gameId, callBack){
-	var options = {
-		"host": "localhost",
-		"port": "3001",
-		"path": "/games/" + gameId,
-		"method": "GET"
-	};
-	var request = http.request(options, function(response){
-		response.setEncoding('utf8');
-		response.on('data', function(data){
-			callBack(JSON.parse(data));
-		});
-	});
-
-	request.end();
-
-}
-
-var makeMove = function(gameId, move, callBack){
-	var options = {
-		"host": "localhost",
-		"port": "3001",
-		"path": "/games/" + gameId + "/moves/" + move,
-		"method": "PUT"
-	}
-	var request = http.request(options, function(response){
-		response.setEncoding('utf8');
-		response.on('data', function(data){
-			callBack(JSON.parse(data));
-		});
-	});
-	request.write("");
-	request.end();
-}
+var RECEIVE_CREATE_GAME = 'new game';
+var EMIT_GAME_CREATED = 'created game';
+var RECEIVE_MAKE_MOVE = 'move';
+var EMIT_MOVE_MADE = 'updated game';
 
 io.on('connection', function(socket){
 	
 	console.log('a user connected');
 	
-	socket.on('new game', function(data){
-		createNewGame(function(game){
-			socket.emit('created game', game)
+	socket.on(RECEIVE_CREATE_GAME, function(data){
+		gameManager.createNewGame(function(game){
+			socket.emit(EMIT_GAME_CREATED, game)
 		})
 	});
 
-	socket.on('random move', function(gameId){
-		getGame(gameId, function(game){
-			makeMove(gameId, game.moves[0], function(game){
-				socket.emit('updated game', game);
+	socket.on(RECEIVE_MAKE_MOVE, function(data){
+		gameManager.getGame(data.gameid, function(game){
+			gameManager.makeMove(data.gameid, data.move, function(game){
+				socket.emit(EMIT_MOVE_MADE, game);
 			});
 		});
 	})
