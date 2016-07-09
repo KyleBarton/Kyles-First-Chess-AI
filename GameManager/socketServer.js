a4d371b1551432bb31cc8ddc19bee62b8c71f651
Kyle Barton <kjbarton4@gmail.com>
2016-07-09 09:39:51 -0700

    got a test client up and running

--- GameManager/socketServer.js
+++ GameManager/socketServer.js
@@ -1,10 +1,17 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.use(express.static('../TestClient'));
 
 io.on('connection', function(socket){
 	console.log('a user connected');
 	socket.on('disconnect', function(){
 		console.log('a user disconnected');
 	})
+});
+
+http.listen("3002", function(){
+	console.log("Listening on port 3002");
 });
