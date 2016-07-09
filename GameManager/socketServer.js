0c81736c7f73de3e8aba257dee84e0f564f0d9f6
Kyle Barton <kjbarton4@gmail.com>
2016-07-09 09:23:06 -0700

    building the test out like the tutorial

--- GameManager/socketServer.js
+++ GameManager/socketServer.js
@@ -1,1 +1,10 @@
-//this will be socket server.js
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('a user disconnected');
+	})
+});
